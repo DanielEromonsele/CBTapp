35a0e0be80aabf0825637097501f732ac45c7a4d
DanielEromonsele <greatereromonsele01@gmail.com>
2025-01-09 01:07:33 +0100

    auth Ui ready

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img2 from "../../assets/Exams-bro.png";
 
-const Login = () => {
-  return <div>Login</div>;
+const Register = () => {
+  return (
+    <div className="bg-green-400 min-h-screen w-full flex items-center justify-center">
+      <div className="min-w-[70%] h-[750px] border bg-white">
+        <div className="m-8 flex flex-wrap items-center justify-between h-[70px]">
+          <div>
+            <h1 className="text-[20px] font-semibold">
+              Open <span className="text-green-600 font-semibold">Test</span>
+            </h1>
+          </div>
+          <Link to="/auth/register">
+            <div className="text-[20px] text-green-600 font-semibold border border-green-600 px-4 py-2 flex items-center justify-center rounded-lg">
+              Register Here
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between m-10 mt-8 gap-6 lg:gap-0">
+          <div className="w-full lg:w-[48%] min-h-[450px] flex flex-col justify-center items-center text-center">
+            <div className="text-[40px] text-green-500 font-semibold mb-2">
+              Welcome Back😊
+            </div>
+            <div className="text-[20px] text-gray-400 font-semibold mb-6">
+              Login To Your Account.
+            </div>
+            <form className="w-full max-w-md space-y-6">
+              {" "}
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="w-full h-[50px] rounded-lg text-gray-600 font-semibold border px-3 border-gray-400 bg-white outline-none focus:ring-green-400 focus:ring-2 transition-all duration-300"
+              />
+              <input
+                type="password"
+                placeholder=" Password"
+                className="w-full h-[50px] rounded-lg text-gray-600 font-semibold border px-3 border-gray-400 bg-white outline-none focus:ring-2 focus:ring-green-400 transition-all duration-300"
+              />
+              <button className="w-full h-[50px] bg-green-400 text-white font-semibold rounded-lg lg:hover:scale-105 hover:bg-green-500 transition-all duration-300">
+                Register
+              </button>
+              <div className="">
+                Don't have an account,
+                <Link to="/auth/register"> Register Here</Link>
+              </div>
+            </form>
+          </div>
+          <div
+            className="w-full lg:w-[47%] min-h-[300px] lg:min-h-[450px] bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${img2})`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -9,7 +9,7 @@ const Register = () => {
         <div className="m-8 flex flex-wrap items-center justify-between h-[70px]">
           <div>
             <h1 className="text-[20px] font-semibold">
-              Open <span className="text-green-600 font-semibold">Test</span>
+              Take A <span className="text-green-600 font-semibold">Test</span>
             </h1>
           </div>
           <Link to="/auth/login">
@@ -20,38 +20,39 @@ const Register = () => {
         </div>
 
         <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between m-10 mt-8 gap-6 lg:gap-0">
-          <div className="w-full lg:w-[48%] min-h-[450px] flex flex-col justify-center">
-            <div className="text-[40px] text-green-500 font-semibold">
+          <div className="w-full lg:w-[48%] min-h-[450px] flex flex-col justify-center items-center text-center">
+            <div className="text-[40px] text-green-500 font-semibold mb-2">
               Register
             </div>
-            <div className="text-[20px] text-gray-400 font-semibold">
+            <div className="text-[20px] text-gray-400 font-semibold mb-6">
               Sign up to get started
             </div>
-            <form action="Submit" className="m-3">
+            <form className="w-full max-w-md space-y-6">
+              {" "}
+              {/* Increased spacing */}
               <input
                 type="text"
                 placeholder="Insert Your Name"
-                className="w-full my-4 h-[50px] rounded-lg text-gray-600 font-semibold border pl-3 border-gray-400 bg-white outline-none focus:ring-green-400 focus:ring-2 transition-all duration-300"
+                className="w-full h-[50px] rounded-lg text-gray-600 font-semibold border px-3 border-gray-400 bg-white outline-none focus:ring-green-400 focus:ring-2 transition-all duration-300"
               />
               <input
                 type="email"
                 placeholder="Email Address"
-                className="w-full my-4 h-[50px] rounded-lg border text-gray-600 font-semibold pl-3 border-gray-400 bg-white outline-none focus:ring-green-400 focus:ring-2 transition-all duration-300"
+                className="w-full h-[50px] rounded-lg text-gray-600 font-semibold border px-3 border-gray-400 bg-white outline-none focus:ring-green-400 focus:ring-2 transition-all duration-300"
               />
               <input
                 type="password"
                 placeholder="Input Password"
-                className="w-full my-4 h-[50px] rounded-lg text-gray-600 font-semibold border pl-3 border-gray-400 bg-white outline-none focus:ring-2 focus:ring-green-400 transition-all duration-300"
+                className="w-full h-[50px] rounded-lg text-gray-600 font-semibold border px-3 border-gray-400 bg-white outline-none focus:ring-2 focus:ring-green-400 transition-all duration-300"
               />
-              <button className="w-full h-[50px] bg-green-400 flex items-center justify-center rounded-lg pl-3 text-white transition-all duration-300 text-[20px] font-semibold hover:scale-105">
+              <button className="w-full h-[50px] bg-green-400 text-white font-semibold rounded-lg lg:hover:scale-105 hover:bg-green-500 transition-all duration-300">
                 Register
               </button>
-              <button className="w-full h-[50px] bg-red-400 flex items-center justify-center rounded-lg pl-3 text-white transition-all duration-300 text-[20px] font-semibold hover:scale-105 mt-3">
+              <button className="w-full h-[50px] bg-red-400 text-white font-semibold rounded-lg lg:hover:scale-105 hover:bg-red-500 transition-all duration-300">
                 Sign In With Google
               </button>
             </form>
           </div>
-
           <div
             className="w-full lg:w-[47%] min-h-[300px] lg:min-h-[450px] bg-cover bg-center"
             style={{
